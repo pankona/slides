88a55714062db6be5f5bfb901152ee31ab867dae
pankona <yosuke.akatsuka@gmail.com>
2018-10-15 11:49:12 +0900

    support to generate gitbook project with specified name

--- materials/gen_gitbook_proj.sh
+++ materials/gen_gitbook_proj.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
-if [ -z $OUTDIR ]; then
-  OUTDIR=$(mktemp -d)
-fi
+TMPDIR=$(mktemp -d)
 
-ln -s ../_gitbook_template/Makefile $OUTDIR/Makefile
-cp -r ./_gitbook_template/src      $OUTDIR/.
-mv $OUTDIR .
-echo "gitbook project generated on $OUTDIR"
+ln -s ../_gitbook_template/Makefile $TMPDIR/Makefile
+cp -r ./_gitbook_template/src       $TMPDIR/.
+
+if [ -z $OUTDIR ]; then
+  mv -f $TMPDIR .
+  echo "gitbook project generated on $(basename $TMPDIR)"
+else
+  mv -f $TMPDIR $OUTDIR
+  echo "gitbook project generated on $OUTDIR"
+fi
