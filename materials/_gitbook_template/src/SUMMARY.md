17e722d3470a7c9ae9d3dea3b6602dee11218636
pankona <yosuke.akatsuka@gmail.com>
2018-10-15 11:39:21 +0900

    add gitbook project generator

---
+++ materials/_gitbook_template/src/SUMMARY.md
@@ -0,0 +1,4 @@
+# Summary
+
+* [本資料について](README.md)
+
---
+++ materials/gen_gitbook_proj.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+if [ -z $OUTDIR ]; then
+  OUTDIR=$(mktemp -d)
+fi
+
+ln -s ../_gitbook_template/Makefile $OUTDIR/Makefile
+cp -r ./_gitbook_template/src      $OUTDIR/.
+mv $OUTDIR .
+echo "gitbook project generated on $OUTDIR"
